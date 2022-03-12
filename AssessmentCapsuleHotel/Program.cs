f746e8ea30718f03bb32a334843bc2d2bcde6671
Aaron Stolle <aaronstolle@gmail.com>
2022-03-12 12:05:30 -0600

    start up method created

--- AssessmentCapsuleHotel/Program.cs
+++ AssessmentCapsuleHotel/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string input = null;
+
+bool validInput = false;
+
+
+Console.Write(@"Welcome to Capsule-Capsule.
+===========================
+Enter the number of capsules available: ");
+input = Console.ReadLine();
+
+do
+{
+    if (int.TryParse(input, out int result))
+    {
+        if (result > 0)
+        {
+            string[] rooms = new string[result];
+            Console.Write(@$"
+There are {result} unoccupied rooms ready to be booked.");
+            // open the main menu
+            break;
+        }
+        else
+        {
+            Console.Write(@"
+Your input must be a positive number!
+Enter the number of rooms available: ");
+            input = Console.ReadLine();
+        }
+
+    }
+    else
+    {
+        Console.Write(@"
+Your input must be a positive number!
+Enter the number of rooms available: ");
+        input = Console.ReadLine();
+    }
+} while (!validInput);
+
+
