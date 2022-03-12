1d7447ee2974718aad6836934f7af58579519485
Aaron Stolle <aaronstolle@gmail.com>
2022-03-12 17:25:49 -0600

    Created Guest Menu Selector

--- AssessmentCapsuleHotel/Program.cs
+++ AssessmentCapsuleHotel/Program.cs
@@ -1,41 +1,96 @@
-﻿string input = null;
-
-bool validInput = false;
-
-
-Console.Write(@"Welcome to Capsule-Capsule.
+﻿Console.Write(@"Welcome to Capsule-Capsule.
 ===========================
 Enter the number of capsules available: ");
-input = Console.ReadLine();
+int RoomsTotal = PositiveNumValidation("");
+string[] rooms = new string[RoomsTotal];
+Console.Write(@$"
+There are {RoomsTotal} unoccupied rooms ready to be booked.");
 
-do
+static int GuestMenuSelection(string prompt)
 {
-    if (int.TryParse(input, out int result))
+    int GuestMenuChoice = 0;
+
+    bool validChoice = false;
+
+
+    do
     {
-        if (result > 0)
+        Console.Write(@"
+Guest Menu
+==========
+1. Check In
+2. Check Out
+3. View Guests
+4. Exit
+Choose on option [1-4]: ");
+        GuestMenuChoice = PositiveNumValidation("");
+        if (GuestMenuChoice > 4)
         {
-            string[] rooms = new string[result];
-            Console.Write(@$"
-There are {result} unoccupied rooms ready to be booked.");
-            // open the main menu
+            Console.Write(@"That Entry is invalid.
+Please Enter a choice between 1-4: ");
+
+        }
+        else if (GuestMenuChoice == 1)
+        {
+            //Open Check In Method
+            break;
+        }
+        else if (GuestMenuChoice == 2)
+        {
+            //Open Check Out Method
+            break;
+        }
+        else if (GuestMenuChoice == 3)
+        {
+            //Open View Guests Method
             break;
         }
         else
         {
+            //Open Exit Method
+            break;
+        }
+    } while (!validChoice);
+    return GuestMenuChoice;
+}
+
+int GuestSelection = GuestMenuSelection("");
+Console.WriteLine($"Your choice was: {GuestSelection}");
+
+
+static int PositiveNumValidation(string prompt)
+{
+    string input = null;
+
+    bool validInput = false;
+
+    input = Console.ReadLine();
+
+    int result = 0; 
+
+    do
+    {
+        if (int.TryParse(input, out result))
+        {
+            if (result > 0)
+            {
+                break;
+            }
+            else
+            {
+                Console.Write(@"
+You must enter a positive number: ");
+                input = Console.ReadLine();
+            }
+
+        }
+        else
+        {
             Console.Write(@"
-Your input must be a positive number!
-Enter the number of rooms available: ");
+You must a positive number: ");
             input = Console.ReadLine();
         }
-
-    }
-    else
-    {
-        Console.Write(@"
-Your input must be a positive number!
-Enter the number of rooms available: ");
-        input = Console.ReadLine();
-    }
-} while (!validInput);
-
+    } while (!validInput);
+    return result;
+} 
 
